a01326c43beafaeacb3a80259d927b9d078cff37
Joe Rumsey <gitub@rumsey.org>
2011-10-12 00:58:56 -0700

    Renders via templates
    "Headers" renamed to sources, and an object for each source
    
    Both included by and includes now listed

--- headerchamp.py
+++ headerchamp.py
@@ -5,7 +5,21 @@ import re
 import argparse
 inc = re.compile('^\s*#include\s*[<"](.*)[>"]')
 
-headers = {}
+class Source:
+    def __init__(self, name, count, size):
+        self.name = name
+        self.count = count
+        self.size = size
+        self.included_by = set()
+        self.includes = set()
+
+    def finalize(self):
+        self.total_cost = self.count * self.size
+
+    def add_included_by(self, included_by):
+        self.included_by.add(included_by)
+
+sources = {}
 def file_size(file):
     f = open(file, 'r')
     count = 0
@@ -15,21 +29,25 @@ def file_size(file):
     return count
 
 def add_header(header, included_by):
-    if header in headers:
-        headers[header]['count'] += 1
+    if not header in sources:
+        print 'added: ' + header
+        h = Source(header, 0, file_size(header))
+        sources[header] = h
+        parse(header, os.path.split(header)[0], True)
     else:
-        headers[header] = {
-            'count': 1,
-            'size': file_size(header),
-            'included_by': set()
-            }
+        h = sources[header]
+        
+    h.count += 1
+    if included_by:
+        h.add_included_by(included_by)
 
-        parse(header, os.path.split(header)[0])
+def parse(filename, in_dir, recursed):
+    filename = os.path.normpath(filename)
+    if not filename in sources:
+        sources[filename] = Source(filename, 0, 0)
 
-    if not included_by in headers[header]['included_by']:
-        headers[header]['included_by'].add(included_by)
+    src = sources[filename]
 
-def parse(filename, in_dir):
     #print filename
     f = open(filename, 'r')
     for line in f:
@@ -37,11 +55,12 @@ def parse(filename, in_dir):
         if m:            
             for id in [in_dir] + include_dirs:
                 found = False
-                chkpath = os.path.join(id, m.group(1))
+                chkpath = os.path.normpath(os.path.join(id, m.group(1)))
                 if os.path.exists(chkpath):
                     #print chkpath
                     found = True
-                    add_header(chkpath, filename)
+                    add_header(chkpath, os.path.normpath(filename))
+                    src.includes.add(chkpath)
                     break
             if not found and args['warn_missing_files']:
                 print m.group(1) + ' was not found while parsing %s in %s' % (filename, in_dir)
@@ -51,7 +70,8 @@ def parse(filename, in_dir):
 def walk(dirname):
     for dir, dirs, files in os.walk(dirname):
         for file in [f for f in files if f.endswith('.cpp')]:
-            parse(os.path.join(dir, file), dir)
+            #print os.path.join(dir, file)
+            parse(os.path.join(dir, file), dir, False)
 
 sys_dirs=['/usr/include', '/usr/local/include']
 include_dirs = []
@@ -72,9 +92,12 @@ def run():
     os.chdir(args['projectdir'])
     walk('.')
 
+    for h in sources.values():
+        h.finalize()
+
 if __name__ == "__main__":
     run()
 
-    for k,v in headers.items():
-        print '%d %d %d %s' %(v['count'], v['size'], v['count'] * v['size'], k)
+    for k,v in sources.items():
+        print '%d %d %d %s' %(v.count, v.size, v.count * v.size, k)
 
--- server.py
+++ server.py
@@ -1,53 +1,35 @@
 
 import tornado.ioloop
 import tornado.web
+import tornado.template
 import headerchamp
 
-from operator import itemgetter
+from operator import itemgetter, attrgetter
 
 HEADER = '<html><body>'
 FOOTER = '</body></html>'
 
-headers = None
+sources = None
+
+loader = tornado.template.Loader('templates')
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        global headers
-        self.write(HEADER)
-
-        self.write('<table>\n')
-        if not headers:
-            headers = []
-            for k,v in headerchamp.headers.items():
-                headers.append((k, v['count'], v['size'], v['count'] * v['size']))
+        global sources
+        if not sources:
+            sources = []
+            for k,v in headerchamp.sources.items():
+                sources.append(v)
             
-            headers.sort(key=itemgetter(3), reverse=True)
+            sources.sort(key=attrgetter('total_cost'), reverse=True)
         
-        for h in headers:
-            self.write('<tr><td>%d</td><td>%d</td><td>%d</td><td><a href="/file/%s">%s</a></td></tr>\n' % (h[1], h[2], h[3], h[0], h[0]))
-        self.write('</table>')
-
-        self.write(FOOTER)
+        self.write(loader.load('index.html').generate(sources=sources))
 
 class FileHandler(tornado.web.RequestHandler):
     def get(self, filename):
-        global headers
-        self.write(HEADER)
-        self.write('<H1>%s</H1>\n' % (filename))
-        self.write('<table>')
-        info = headerchamp.headers[filename]
-        self.write('<tr><td>Lines</td><td>%d</td></tr>' % (info['size']))
-        self.write('<tr><td>Count</td><td>%d</td></tr>' % (info['count']))
-        self.write('<tr><td>Total Count</td><td>%d</td></tr>' % (info['count'] * info['size']))
-        self.write('</table>\n')
-        self.write('<H2>Included by</H2>\n')
-        for ib in headerchamp.headers[filename]['included_by']:
-            if ib in headerchamp.headers:
-                self.write('<p><a href="/file/%s">%s</a></p>\n' % (ib, ib))
-            else:
-                self.write('<p>%s</p>\n' % (ib))
-
-        self.write(FOOTER)
+        global sources
+        header = headerchamp.sources[filename]
+        self.write(loader.load('file.html').generate(header=header, all_sources = headerchamp.sources))
 
 application = tornado.web.Application([
         (r'/', MainHandler),
